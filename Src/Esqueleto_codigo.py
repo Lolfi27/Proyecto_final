35aca73e912eb7c9efb390a1ab8a8dd9fc7c02b3
Lolfi_27 <dial.he.gtz@gmail.com>
2025-08-28 14:50:01 -0600

    Prototipo Esqueleto_codigo

--- Src/Esqueleto_codigo.py
+++ Src/Esqueleto_codigo.py
@@ -1,1 +1,16 @@
-#Aqui se hace el esqueleto del codigo
+objetos_a_registrar = int(input("Cuantos objetos se van a registrar"))
+# Variables de input
+objetos_frios = int(input("Cuantos objetos almacenados en frio habra?"))
+objetos_snacks = int(input("Cuantas chucherias habra?"))
+objetos_enlatados = int(input("Cuantos enlatados habra?"))
+objetos_limpieza = int(input("Cuantos objetos de limpieza habra?"))
+objetos_mascotas = int(input("Cuantos objetos de mascotas habra?"))
+objetos_calientes = int(input("Cuantos objetos calientes habra?"))
+
+# Sistema de separación y conteo individual por tipo de objeto
+# Aqui se separa cada objeto de cada categoria para un control más preciso
+# ej. "Cuantas papas habra?" Registrado como 1 interger de objetos_snacks
+
+# Conexión de datos a excel para un manejo de codigo
+# Se conecta el codigo a una tabla de excel con un facil manejo y función
+
