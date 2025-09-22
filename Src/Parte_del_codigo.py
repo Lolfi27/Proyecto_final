96acaad3e521fdd507aa0aff0035338facd614f4
Regina <113712579+Rznahdz@users.noreply.github.com>
2025-09-22 14:59:37 -0600

    Update Parte_del_codigo.py
    Día, mes & año - reg

--- Src/Parte_del_codigo.py
+++ Src/Parte_del_codigo.py
@@ -15,7 +15,37 @@ def agregar_inventario():
                 piezas = int(input("Cantidad: "))
             except ValueError:
                 piezas = 0
-            inventario[categoria].append({"nombre": nombre, "cantidad": piezas})
+#VERIFICAR DÍA
+            while True:
+                try:
+                    d = int(input("Día de registro del producto (1-31): "))
+                    if 1 <= d <= 31:
+                        return d
+                    else:
+                        print("El dia tiene que estar entre 1-31")
+                except ValueError:
+                    print("Por favor, ingrese solo numeros enteros")
+#VERIFICAR MES
+            while True:
+                try:
+                    m = int(input("Mes de registro del producto (1-12): "))
+                    if 1 <= m <= 12:
+                        return m
+                    else:
+                        print("El mes tiene que estar entre 1 y 12")
+                except ValueError:
+                    print("Por favor, ingrese solo numeros enteros")
+#VERIFICAR AÑO
+            while True:
+                try:
+                    a = int(input("Año de registro del producto: "))
+                    if a > 0:
+                        return a
+                    else:
+                        print("El año tiene que ser mayor a 0")
+                except ValueError:
+                    print("Por favor, ingrese solo numeros enteros")
+                
+            inventario[categoria].append({"nombre": nombre, "cantidad": piezas, "fecha": (d, m, a)})
             print("\n¡Sus productos fueron registrados exitosamente!")
     return inventario
-
